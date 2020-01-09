d81c910e17f40f6c97aaa6e9600f5a15bfdbcf22
Harriydaran <harriydaranr@gmail.com>
2020-01-09 21:37:34 +0000

    Remove Template Items

---
+++ app-dash/src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar">
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
