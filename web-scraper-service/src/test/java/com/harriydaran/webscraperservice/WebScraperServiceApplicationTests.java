3464ea69190760ced4a44ffa583b6ac23cb86c68
Harriydaran <harriydaranr@gmail.com>
2020-01-09 19:26:32 +0000

    Init Project

---
+++ web-scraper-service/src/test/java/com/harriydaran/webscraperservice/WebScraperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harriydaran.webscraperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScraperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
