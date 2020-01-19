6dd679d8d9042766b942b7b87bcb9245f245cfe8
Harriydaran <harriydaranr@gmail.com>
2020-01-19 19:19:55 +0000

    Added RestTemplate to call user-service

---
+++ api-gateway-service/src/main/java/com/harriydaran/apigatewayservice/TestClient.java
@@ -0,0 +1,17 @@
+package com.harriydaran.apigatewayservice;
+
+import org.springframework.web.client.RestTemplate;
+
+public class TestClient {
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        String id = "3";
+        User user = restTemplate.getForObject("http://localhost:8080/api/v1/users/" + id, User.class);
+        System.out.println(user.getFirstName());
+        System.out.println(user.getAddress().getCountry());
+    }
+
+
+
+}
