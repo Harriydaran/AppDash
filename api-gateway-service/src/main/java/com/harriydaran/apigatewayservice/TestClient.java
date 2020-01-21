423f7ac48f42dbd5d9673bab0ee406c6e16e15f8
Harriydaran <harriydaranr@gmail.com>
2020-01-21 17:51:32 +0000

    Added Discovery Service

--- api-gateway-service/src/main/java/com/harriydaran/apigatewayservice/TestClient.java
+++ api-gateway-service/src/main/java/com/harriydaran/apigatewayservice/TestClient.java
@@ -7,7 +7,7 @@ public class TestClient {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
         String id = "3";
-        User user = restTemplate.getForObject("http://localhost:8080/api/v1/users/" + id, User.class);
+        User user = restTemplate.getForObject("http://user-service:8080/api/v1/users/" + id, User.class);
         System.out.println(user.getFirstName());
         System.out.println(user.getAddress().getCountry());
     }
