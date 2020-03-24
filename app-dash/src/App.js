e13b2c5094249304b6ca2b5b37a1ea9401dc1575
Harriydaran <harriydaranr@gmail.com>
2020-03-24 19:38:02 +0000

    Removed boiler plate code

--- app-dash/src/App.js
+++ app-dash/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
-import Sidebar from "./Components/Sidebar";
-import DashButton from "./Components/DashButton"
 
 class App extends React.Component {
-
-    componentDidMount() {
-
-    }
 
     render() {
         return (
             <div className="App">
-                <Sidebar/>
-                <div className="main">
-                    <DashButton name={"Pink"}/>
-                </div>
             </div>
         );
     }
